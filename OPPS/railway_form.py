3b6728413f403202ec83494d547067e33fc953f8
“pritesh” <pritesvadoliya5@gmail.com>
2022-06-24 15:33:43 +0400

    prectic_set

--- OPPS/railway_form.py
+++ OPPS/railway_form.py
@@ -1,7 +1,8 @@
 class RailwayForm:
     formType="RailwayForm"
     def printData(self):
-        print(f"train name is: {self.train}\nname is: {self.name}")
+        print(f"train name is: {self.train}")
+        print(f"name is: {self.name}")
 harryAppliCation= RailwayForm()
 harryAppliCation.name="Harry"
 harryAppliCation.train="Rajdhani Express"
