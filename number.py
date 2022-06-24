7a12a929ae4dd4df06132908d9683334c8a19e05
“pritesh” <pritesvadoliya5@gmail.com>
2022-06-24 10:11:12 +0400

    number

---
+++ number.py
@@ -0,0 +1,12 @@
+num = int(input("enter your number:"))
+i = 100
+sum = 0
+while i<=num:
+          sum = sum + i
+          i = i + 1
+print(f"the sum of first {num} natural numbers is {sum}")
+
+
+if(0<500):
+    print("sum is right")
+
