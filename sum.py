81bfd21208f8103698ad07c45bcc0ce2983049be
“pritesh” <pritesvadoliya5@gmail.com>
2022-06-24 10:35:14 +0400

    sum

---
+++ sum.py
@@ -0,0 +1,8 @@
+a=int(input("Enter your 1 number"))
+b=int(input("Enter your 2 number"))
+if(0>500):
+    print("This sum is right")
+else:
+    print("this sum is not right")
+
+print("the sum of a and b is :",a+b)
