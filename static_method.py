c306a5d9302e8e19a35e71e69e78266413137f64
“pritesh” <pritesvadoliya5@gmail.com>
2022-06-24 15:26:17 +0400

    static_method

--- static_method.py
+++ static_method.py
@@ -1,13 +1,3 @@
-# class Employee:
-#     company="Goggle"
-    
-#     def getSalary(self):
-#         print(f"Salary for this employee working in {self.company}is {self.Salary}")
-
-# harry=Employee()
-# harry.Salary=100000
-# harry.getSalary()
-
 from datetime import datetime
 
 
