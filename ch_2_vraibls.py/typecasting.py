741f763c21015f412c35fedb5c7653fb320e3e4e
“pritesh” <pritesvadoliya5@gmail.com>
2022-06-30 12:20:43 +0530

    project

---
+++ ch_2_vraibls.py/typecasting.py
@@ -0,0 +1,5 @@
+a=325528
+b="komal"
+
+print(type(a))
+print(type(b))
---
+++ project/project_5_.py
@@ -0,0 +1,8 @@
+print("Hello Python Programmer\n")
+
+a=88888
+b=968.55
+
+print("This value is a and b : ",a+b )
+print("******************************")
+print("This value is a and b :",a/b)
