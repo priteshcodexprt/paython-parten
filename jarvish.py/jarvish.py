2d9e230566e0f1e52dc93e31952e8af963f5fac0
“pritesh” <pritesvadoliya5@gmail.com>
2022-07-01 09:32:40 +0530

    project

--- jarvish.py/jarvish.py
+++ jarvish.py/jarvish.py
@@ -1,119 +1,52 @@
-import pyttsx3 
-import speech_recognition as sr 
-import datetime
-import wikiPedia 
-import webbrowser
+import speech_recognition as sr
+from time import ctime
+import time
 import os
-import smtplib
+from gtts import gTTS
 
-engine = pyttsx3.init('sapi5')
-voices = engine.getProperty('voices')
-print(voices[1].id)
-engine.setProperty('voice', voices[1].id)
+def speak(audioString):
+    print(audioString)
+    tts = gTTS(text=audioString, lang='en')
+    tts.save("audio.mp3")
+    os.system("mpg321 audio.mp3")
 
-
-def speak(audio):
-    engine.say(audio)
-    engine.runAndWait()
-
-
-def wishMe():
-    hour = int(datetime.datetime.now().hour)
-    if hour>=0 and hour<12:
-        speak("Good Morning!")
-
-    elif hour>=12 and hour<18:
-        speak("Good Afternoon!")   
-
-    else:
-        speak("Good Evening!")  
-
-    speak("Hello Neel ,how may I help you")       
-
-def takeCommand():
-    #It takes microphone input from the user and returns string output
-
+def recordAudio():
+    # Record Audio
     r = sr.Recognizer()
     with sr.Microphone() as source:
-        print("Listening...")
-        r.pause_threshold = 2
-        audio = r.listen(source)
+        print("Say something!")
+    audio = r.listen(source)
 
+    # Speech recognition using Google Speech Recognition
+    data = ""
     try:
-        print("Recognizing...")    
-        query = r.recognize_google(audio, language='en-in')
-        print(f"User said: {query}\n")
+        # Uses the default API key
+        # To use another API key: `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
+        data = r.recognize_google(audio)
+        print("You said: " + data)
+    except sr.UnknownValueError:
+        print("Google Speech Recognition could not understand audio")
+    except sr.RequestError as e:
+        print("Could not request results from Google Speech Recognition service; {0}".format(e))
 
-    except Exception as e:
-        # print(e)    
-        print("Say that again please...")  
-        return "None"
-    return query
+    return data
 
-def sendEmail(to, content):
-    server = smtplib.SMTP('smtp.gmail.com', 587)
-    server.ehlo()
-    server.starttls()
-    server.login('Your Email', 'Your Password')
-    server.sendmail('Your Email', to, content)
-    server.close()
+def jarvis(data):
+    if "how are you" in data:
+        speak("I am fine")
 
-if __name__ == "__main__":
-    wishMe()
-    while True:
-    # if 1:
-        query = takeCommand().lower()
+    if "what time is it" in data:
+        speak(ctime())
 
-        # Logic for executing tasks based on query
-        if 'wikipedia' in query:
-            speak('Searching Wikipedia...')
-            query = query.replace("wikipedia", "")
-            results = wikipedia.summary(query, sentences=10)
-            speak("According to Wikipedia")
-            print(results)
-            speak(results)
+    if "where is" in data:
+        data = data.split(" ")
+        location = data[2]
+        speak("Hold on Frank, I will show you where " + location + " is.")
+        os.system("chromium-browser https://www.google.nl/maps/place/" + location + "/&amp;")
 
-        elif 'open youtube' in query:
-            webbrowser.open("youtube.com")
-
-        elif 'open stackoverflow' in query:
-            webbrowser.open("stackoverflow.com")  
-
-        elif 'open python' in query:
-            webbrowser.open("coursera.org")   
-        elif 'open github' in query:
-            webbrowser.open("github.com")   
-
-        elif 'open mail' in query:
-            webbrowser.open("mail.google.com")   
-      
-
-
-
-        elif 'play music' in query:
-            music_dir = 'C:\\Users\\Admin\\Music\\my.mp3'
-            songs = os.listdir(music_dir)
-            print(songs)    
-            os.startfile(os.path.join(music_dir, songs[0]))
-
-        elif 'the time' in query:
-            strTime = datetime.datetime.now().strftime("%H:%M:%S")    
-            speak(f"Sir, the time is {strTime}")
-
-        elif 'open code' in query:
-            codePath = "E:\\Microsoft VS Code\\Code.exe"
-            os.startfile(codePath)
-
-        elif 'open google' in query:
-            go = "C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe"
-            os.startfile(go)
-
-        elif 'email to me' in query:
-            try:
-                speak("What should I say?")
-                content = takeCommand()
-                to = "you@youremail.com"    
-                sendEmail(to, content)
-                speak("Email has been sent!")
-            except Exception as e:
-                print(e)
+# initialization
+time.sleep(2)
+speak("Hi Frank, what can I do for you?")
+while 1:
+    data = recordAudio()
+    jarvis(data)
