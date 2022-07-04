574f4d45fa19e19cf813876214923c553400854d
“pritesh” <pritesvadoliya5@gmail.com>
2022-07-04 15:38:18 +0530

    jarvis

--- jarvish.py
+++ jarvish.py
@@ -1,3 +1,5 @@
+from email.mime import audio
+from unittest import result
 import pyttsx3
 import speech_recognition as sr
 import datetime
@@ -6,6 +8,8 @@ import time
 import os
 import webbrowser
 import smtpd
+
+import youtube_dl
 
 engine= pyttsx3.init('sapi5')
 voices= engine.getProperty('voices')
@@ -24,4 +28,44 @@ def wishme():
         speak("Good Afternoon!")
     else :
         speak ("Good Morning!")
+    speak("I am jarvis sir . Please  tell me how may i help you ")
+
+def takeCommand():
+
+
+    r=sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Listening...")
+        r.pause_threshold = 1
+        audio = r.listen(source)
+    try :
+        print("Recognizing ...")
+        query =r.recognize_google(audio , language="en-in")
+        print(f"user said :{query}\n")
+
+    except Exception as e:
+        # print(e)
+        print("say that again please ...")
+        return "None"
+    return query
+
+if __name__ == "main ":
+    wishMe()
+    while True:
+        query = takeCommand().lower()
+
+        if 'wikipedia' in query:
+            speak('searching Wikipedia ...')
+            query =query.replace("wikipedia...")
+            result=wikipedia.summary(query,sentence=2)
+            speak("According to Wikipedia  ")
+            print(result)
+            speak(result)
+
+        elif 'open youtube'in query:    
+            webbrowser.open("youtube.com")
+            
+
+
+
 
