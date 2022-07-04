cfb2bc73e97cef424fa4ff735dec20a1efeaab9b
“pritesh” <pritesvadoliya5@gmail.com>
2022-07-04 11:36:21 +0530

    ch_3

---
+++ ch_4.py/list.py
@@ -0,0 +1,10 @@
+# your value 
+a=[5,55,555,5555]
+# printing the value 
+print(a)
+#printing values [0]
+print(a[2])
+# changing the value 
+a[0]=8888
+# printing value 
+print(a)
---
+++ ch_4.py/objectiv.py
@@ -0,0 +1,9 @@
+l1=[9,99,999,9999,99999]
+print(l1)
+# l1.sort
+# l1.reverse()
+# l1.append(999999)
+# l1.extend(9)
+# l1.pop(0)
+l1.remove(99999)
+print(l1)
