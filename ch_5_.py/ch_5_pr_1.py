f64666d4e240347861db3a858f621e9a48dcf5b3
“pritesh” <pritesvadoliya5@gmail.com>
2022-07-05 11:07:25 +0530

    prectic

---
+++ ch_5_.py/ch_5_pr_1.py
@@ -0,0 +1,11 @@
+myDict= {
+    "vastu":"item",
+    "pankha":"fan",
+    "phoen":"mobile",
+    "khurshi":"chair",
+    "pani":"water",
+}
+
+print("options are",myDict.keys())
+a=input("Enter the  hindi word:\n")
+print("The meaning of your word is :",myDict[a])
--- jarvish.py
+++ jarvish.py
@@ -64,7 +64,13 @@ if __name__ == "main ":
 
         elif 'open youtube'in query:    
             webbrowser.open("youtube.com")
-            
+
+        elif 'open google' in query:
+            webbrowser.open("google.com")
+        elif 'open stackoverflow' in query:
+            webbrowser.open ("stackoverflow.com")
+        
+     
 
 
 
