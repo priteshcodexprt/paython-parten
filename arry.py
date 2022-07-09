a56683514d5ddaad0ae40800ea86ba81491dd955
“pritesh” <pritesvadoliya5@gmail.com>
2022-07-09 12:58:55 +0530

    task

---
+++ arry.py
@@ -0,0 +1,26 @@
+# Define the list
+listData = ["Dell", "HP", "Lenovo", "Aus"]
+
+# Insert data using append method
+listData.append("Toshiba")
+
+# Display the list after insert
+print("The list elements are")
+
+for i in range(0, len(listData)):
+  print(listData[i])
+
+
+
+
+# Declare list
+listData = [89, 56, 90, 34, 89, 12]
+
+# Insert data in the 2nd position
+listData.insert(2, 23)
+
+# Displaying list after inserting
+print("The list elements are")
+
+for i in range(0, len(listData)):
+  print(listData[i])
---
+++ task/task.py
@@ -0,0 +1,159 @@
+# # myDict={}
+# # print("PRINT1\n",myDict)
+
+
+# # myDict["AAAAA"]="AAAAA"
+# # myDict["BBBBB"]={}
+# # myDict["CCCCC"]={}
+# # myDict["DDDDD"]={}
+
+
+# # print("PRINT2\n",myDict)
+
+# # myDict["AAAAA"]="AAAAA"
+# # myDict["BBBBB"]={}
+# # myDict["BBBBB"]["CCCC"] = "CCCC"
+# # myDict["BBBBB"]["DDDD"]="DDDD"
+# # myDict["CCCCC"]={}
+# # myDict["CCCCC"]["DDDD"]="DDDD"
+# # myDict["CCCCC"]["EEEE"]={}
+# # myDict["DDDDD"]={}
+# # myDict["DDDDD"]["EEEE"]="EEEE"
+# # myDict["DDDDD"]["FFFF"]={}
+# # myDict["DDDDD"]["GGGG"]={}
+
+# # print("PRINT3\n",myDict)
+
+
+# # myDict["AAAAA"]="AAAAA"
+# # myDict["BBBBB"]={}
+# # myDict["BBBBB"]["CCCC"] = "CCCC"
+# # myDict["BBBBB"]["DDDD"]="DDDD"
+# # myDict["CCCCC"]={}
+# # myDict["CCCCC"]["DDDD"]="DDDD"
+# # myDict["DDDDD"]={}
+# # myDict["DDDDD"]["EEEE"]="EEEE"
+# # myDict["DDDDD"]["FFFF"]={}
+# # myDict["DDDDD"]["GGGG"]={}
+# # myDict["GGGG"]={}
+# # myDict["GGGG"]["HHH"]={}
+
+
+# # print("PRINT4\n",myDict)
+
+
+# # myDict["AAAAA"]="AAAAA"
+# # myDict["BBBBB"]={}
+# # myDict["BBBBB"]["CCCC"]="CCCC"
+# # myDict["BBBBB"]["DDDD"]="DDDD"
+# # myDict["CCCCC"]={}
+# # myDict["CCCCC"]["DDDD"]="DDDD"
+# # myDict["CCCCC"]["EEEE"]={}
+# # myDict["CCCCC"]["EEEE"]["FFF"]="FFF"
+# # myDict["DDDDD"]={}
+# # myDict["DDDDD"]["EEEE"]="EEEE"
+# # myDict["DDDDD"]["FFFF"]={}
+# # myDict["DDDDD"]["FFFF"]["GGG"]="GGG"
+# # myDict["GGGG"]={}
+# # myDict["GGGG"]["HHH"]={}
+# # myDict["GGGG"]["HHH"]["III"]="III"
+# # print("PRINT5\n",myDict)
+
+
+
+
+# CODE REMOVE "D" ALL SPACE 
+
+
+
+# myDict={
+#     "AAAAA": "AAAAA",
+#     "BBBBB": {},
+#     "CCCCC": {},
+#     "DDDDD": {}
+# }
+# D=myDict.pop("DDDDD")
+# print("PRINT1\n",myDict)
+
+# myDict={
+#     "AAAAA": "AAAAA",
+#     "BBBBB": {
+#         "CCCC": "CCCC",
+#         "DDDD": "DDDD"
+#     },
+#     "CCCCC": {
+#         "DDDD": "DDDD",
+#         "EEEE": {}
+#     },
+#     "DDDDD": {
+#         "EEEE": "EEEE",
+#         "FFFF": {},
+#         "GGGG": {}
+#     }
+# }
+
+# myDict.popitem()
+# print("PRINT2\n",myDict)
+# myDict={
+#     "AAAAA": "AAAAA",
+#     "BBBBB": {
+#         "CCCC": "CCCC",
+#         "DDDD": "DDDD"
+#     },
+#     "CCCCC": {
+#         "DDDD": "DDDD",
+#         "EEEE": {
+#             "FFF": "FFF"
+#         }
+#     },
+#     "DDDDD": {
+#         "EEEE": "EEEE",
+#         "FFFF": {
+#             "GGG": "GGG"
+#         },
+#         "GGGG": {
+#             "HHH": {}
+#         }
+#     }
+# }
+
+# myDict.popitem()
+# print("PRINT3\n",myDict)
+# myDict={
+#     "AAAAA": "AAAAA",
+#     "BBBBB": {
+#         "CCCC": "CCCC",
+#         "DDDD": "DDDD"
+#     },
+#     "CCCCC": {
+#         "DDDD": "DDDD",
+#         "EEEE": {
+#             "FFF": "FFF"
+#         }
+#     },
+#     "DDDDD": {
+#         "EEEE": "EEEE",
+#         "FFFF": {
+#             "GGG": "GGG"
+#         },
+#         "GGGG": {
+#             "HHH": {
+#                 "III": "III"
+#             }
+#         }
+#     }
+# }
+
+# myDict.clear()
+# print("PRINT4\n",myDict)
+
+
+# print(myDict)
+
+# myDict={
+#     "AAAAA": "AAAAA",
+#     "BBBBB": {},
+#     "CCCCC": {},
+#     "DDDDD": {}
+# }
+
